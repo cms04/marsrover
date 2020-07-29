e32ad6dd2633204de3756c92171428c258371fc0
cms04 <Christian.Morbach@hhu.de>
2020-07-29 10:51:36 +0200

    Interaktive Befehlseingabe

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
+#include <string.h>
 #include "mars.h"
 
-int main() {
+int main(int argc, const char** argv) {
+    const char* befehle;
+    int i;
     Mars* mars = initalizeMars(20, 80);
+    if (argc == 2) {
+        befehle = *(argv + 1);
+    } else {
+        befehle = "MMMLMMMMMMRBBBBBLMM";
+    }
     printField(mars);
-    moveRover(mars, 'L');
-    printField(mars);
-    moveRover(mars, 'M');
-    moveRover(mars, 'M');
-    printField(mars);
+    for (i = 0; i < strlen(befehle); i++) {
+        moveRover(mars, *(befehle + i));
+        printField(mars);
+    }
     deleteMars(mars);
     return 0;
 }
