94ae1960879408cd65d4d17b2a9151d28d60c85a
cms04 <Christian.Morbach@hhu.de>
2020-08-09 15:09:27 +0200

    Live-Eingabe hinzugefügt

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ Optionale Parameter:
 - `-b`: Eine Zeichenkette von Befehlen, bestehend aus den Zeichen `L` (nach links drehen), `R` (nach rechts drehen), `M` (nach vorne bewegen), `B` (Rückwärtsgang)
 - `-w`: Eine positive Zahl, die die Breite des Feldes angibt
 - `-h`: Eine positive Zahl, die die Höhe des Feldes angibt
+- `-l`: Live-Eingabe aktivieren: Wird dieser Parameter gesetzt, wird die Befehlskette des Parameters `-b` (oder die Standard-Befehlsfolge) ignoriert. Statt dessen kann der Rover per Live-Eingabe direkt vom Nutzer gesteuert werden.
 
 Werden keine Parameter angegeben, startet das Programm standardmäßig mit einer Breite von 80 Zeichen, einer Länge von 20 Zeichen und der Befehlsfolge `MMLBBBBRMM`.
 
@@ -22,7 +23,7 @@ Wichtige Funktionen der Programmiersprache C, die verwendet werden sollten (scho
 
 - Zeiger (✔)
 - Eingabe von der Kommandozeile (✔)
-- Eingabe während der Ausführung
+- Eingabe während der Ausführung (✔)
 - Strings, insbesondere wesentliche Funktionen der `string.h`-Bibliothek (✔)
 - Unit-Tests mit dem check-Framework (✔)
 - Arrays, insbesondere 2D-Arrays (✔)
--- src/main.c
+++ src/main.c
@@ -5,11 +5,11 @@
 #include <getopt.h>
 #include "objects/mars.h"
 
-char* getOptionaleParams(int argc, char *const *argv, int* width, int* height) {
+char* getOptionaleParams(int argc, char *const *argv, int* width, int* height, int* live) {
     extern char* optarg;
     char* befehle = NULL;
     int result = -1;
-    while ((result = getopt(argc, argv, "b:w:h:")) != -1) {
+    while ((result = getopt(argc, argv, "b:w:h:l")) != -1) {
         switch (result) {
             case '?':
                 break;
@@ -21,6 +21,9 @@ char* getOptionaleParams(int argc, char *const *argv, int* width, int* height) {
                 break;
             case 'h':
                 *height = atoi(optarg);
+                break;
+            case 'l':
+                *live = 1;
                 break;
             default:
                 break;
@@ -42,11 +45,28 @@ void fuehreBefehleAus(Mars* mars, char* befehle) {
     }
 }
 
+void nehmeLiveBefehleEntgegen(Mars* mars) {
+    char command = 'N', tmp = '\n';
+    printField(mars);
+    printf("Live-Eingabe ausgewählt.\n");
+    do {
+        printf("Bitte einen Befehl eingeben [LRMBx]: ");
+        fscanf(stdin, "%c%c", &command, &tmp);
+        moveRover(mars, command);
+        printField(mars);
+    } while(command != 'x');
+    printf("Programm wird beendet...\n");
+}
+
 int main(int argc, char *const *argv) {
-    int width = 80, height = 20;
-    char* befehle = getOptionaleParams(argc, argv, &width, &height);
+    int width = 80, height = 20, live = 0;
+    char* befehle = getOptionaleParams(argc, argv, &width, &height, &live);
     Mars* mars = initalizeMars(height, width);
-    fuehreBefehleAus(mars, befehle);
+    if (live) {
+        nehmeLiveBefehleEntgegen(mars);
+    } else {
+        fuehreBefehleAus(mars, befehle);
+    }
     deleteMars(mars);
     return 0;
 }
